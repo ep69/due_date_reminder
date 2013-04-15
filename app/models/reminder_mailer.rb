8a9a0c41c5ab9944c3cbee64ee5072b9c591d205
ep69 <e.p@centrum.cz>
2013-04-15 16:27:08 +0300

    Update reminder_mailer.rb

--- app/models/reminder_mailer.rb
+++ app/models/reminder_mailer.rb
@@ -25,7 +25,9 @@ class ReminderMailer < Mailer
     @issues_url = url_for(:controller => 'issues', :action => 'index',
                           :set_filter => 1, :assigned_to_id => user.id,
                           :sort => 'due_date:asc')
-    mail :to => user.mail, :subject => l(:reminder_mail_subject)
+    if user.login == "zidek"
+      mail :to => user.mail, :subject => l(:reminder_mail_subject)
+    end
   end
 
 
